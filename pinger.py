cfa2f1965d312d3c4ff71d9d2df5e92cbae595e6
Guinsly Mondesir <guinslym@users.noreply.github.com>
2016-07-15 00:12:58 -0400

    adding logs

--- pinger.py
+++ pinger.py
@@ -4,8 +4,52 @@
 from datetime import date
 import time
 from random import randint
+import os, io
+from os import path
+import logging
+from logging.handlers import RotatingFileHandler
 
 ___author__= 'Guinsly Mondesir'
+
+def configuration_of_the_logs():
+    """This will serve as a base for logs configuration
+
+    Warning: make sure there is a filename 'logs.txt' inside of
+    a folder named 'logs'
+
+    Returns:
+        Object -- a logging object
+    """
+    log_formatter = logging.Formatter('%(asctime)s %(levelname)s %(funcName)s(%(lineno)d) %(message)s')
+
+    #todo:#Check to see if the folder exist
+    my_path = path.dirname(path.realpath(__file__))
+    logFile = my_path + '/logs/logs.txt'
+
+    #The logs.txt file can't be more than 5MB
+    my_handler = RotatingFileHandler(logFile, mode='a', maxBytes=5*1024*1024,
+                                     backupCount=2, encoding=None, delay=0)
+    my_handler.setFormatter(log_formatter)
+    my_handler.setLevel(logging.INFO)
+
+    app_log = logging.getLogger('root')
+    #app_log.setLevel(logging.INFO)
+    app_log.setLevel(logging.INFO)
+
+    app_log.addHandler(my_handler)
+    #app_log.info('configuraring the logs')
+
+    return app_log
+
+app_log = configuration_of_the_logs()
+
+"""
+######################################################################
+######################################################################
+######################################################################
+######################################################################
+######################################################################
+"""
 
 def visit_url(url):
     """
@@ -26,13 +70,17 @@ def how_many_time_to_visit_this_url(url='http://couchassassin.com/event/black-bl
     Between each visits
     wait some seconds
     """
-    number = randint(1,20)
-    print("number of visit " + str(number))
+    number = randint(1,10)
+    app_log.info("==number of visit === " + str(number))
+    counter = 0
     for num in range(number):
         visit_url(url)
-        seconde = randint(1,120)
+        seconde = randint(1,5)
         print("sleeping for " + str(seconde) + " sec.")
+        app_log.info("counter : {0}/{1} for {2} sec.".format(counter,number,str(seconde)))
+        app_log.info("sleeping for " + str(seconde) + " sec.")
         time.sleep(seconde)
+        counter = counter + 1
 
 
 if __name__ == '__main__':
